82aeb5bc76b351373f1fd46e1ce96d6e779765b0
Miles Rausch <miles@milesrausch.com>
2015-12-28 22:39:06 -0600

    Updated our Pagespeed task to point to the right site

--- gulpfile.js
+++ gulpfile.js
@@ -219,7 +219,7 @@ gulp.task('deploy', function (cb) {
 // Run PageSpeed Insights
 gulp.task('pagespeed', function (cb) {
   // Update the below URL to the public URL of your site
-  pagespeed.output('example.com', {
+  pagespeed.output('milesrauschfamily.com/ccard/2015', {
     strategy: 'mobile',
     // By default we use the PageSpeed Insights free (no API key) tier.
     // Use a Google Developer API key if you have one: http://goo.gl/RkN0vE
